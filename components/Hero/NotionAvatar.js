d4cbf9d758b9ed8b7a6e5a81fbe62ae909d6c6ff
paulhu2005 <148558658+paulhu2005@users.noreply.github.com>
2023-10-30 11:26:31 +0300

    Update NotionAvatar.js

--- components/Hero/NotionAvatar.js
+++ components/Hero/NotionAvatar.js
@@ -2,15 +2,7 @@
 import * as React from 'react'
 
 const NotionAvatar = (props) => (
-<svg width="107" height="146" xmlns="http://www.w3.org/2000/svg">
- <g>
-  <title>Layer 1</title>
-  <text fill="#6D97AB" stroke="#000" stroke-width="0" x="0" y="29.06148" id="svg_1" font-size="24" font-family="Noto Sans JP" text-anchor="start" xml:space="preserve" transform="matrix(1, 0, 0, 1.03144, 0, -3.58086)">Welcome</text>
-  <text fill="#46673D" stroke="#000" stroke-width="0" x="1" y="80.59453" id="svg_2" font-size="24" font-family="Noto Sans JP" text-anchor="start" xml:space="preserve">Bienvenu</text>
-  <text fill="#426373" stroke="#3B2F0E" stroke-width="0" x="31" y="133.59453" id="svg_3" font-size="24" font-family="Noto Sans JP" text-anchor="start" xml:space="preserve">欢迎</text>
- </g>
 
-</svg>
 )
 
 export default NotionAvatar
